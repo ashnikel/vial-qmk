2272f73b3918a233fa39e9331a49b91368673d65
Victor Medvedik <ashnikel@gmail.com>
2024-08-06 18:19:47 +0300

    wip

--- keyboards/crkbd/keymaps/ashnikel/rules.mk
+++ keyboards/crkbd/keymaps/ashnikel/rules.mk
@@ -7,8 +7,8 @@ RGB_MATRIX_ENABLE   = yes # Can't have RGBLIGHT and RGB_MATRIX at the same time.
 VIALRGB_ENABLE 		= yes
 MOUSEKEY_ENABLE     = no
 OLED_ENABLE         = no
-OLED_DRIVER         = SSD1306
-EXTRAKEY_ENABLE     = no
+OLED_DRIVER         = ssd1306
+EXTRAKEY_ENABLE     = yes
 COMBO_ENABLE        = no
 
 QMK_SETTINGS        = no
